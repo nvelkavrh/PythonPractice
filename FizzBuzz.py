fc0f74158300e96fe39319bc2ea0377bf582c30d
Nick Velkavrh <nvelkavrh@gmail.com>
2012-12-31 22:22:54 -0800

    Added comments to FizzBuzz.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-# Fizz-Buzz in Python
+''' 
+Fizz-Buzz in Python.
+
+Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+'''
 for i in range(1, 101):
     if (i % 3 == 0) and (i % 5 == 0):
         print "FizzBuzz"
